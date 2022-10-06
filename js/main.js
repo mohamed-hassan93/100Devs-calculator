b1eb1159e52b51d68aceeebe20ddd8076f5db410
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-10-05 21:58:48 -0400

    new

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const calculator = {
-  displayValue: '0',
+  displayValue: 0,
   firstOperand: null,
   waitingForSecondOperand: false,
   operator: null,
-}
+};
+
+
 
 function updateDisplay(){
-  document.querySelector('.functionKey').value = calculator.displayValue
+  const displayScreen = document.querySelector('.functionalSpace');
+
+  displayScreen.innerHTML = calculator.displayValue
 }
 
 updateDisplay()
+
+
