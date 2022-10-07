8899b90191059e0020dc92bfe30f5c2bcca63c02
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-10-07 00:31:27 -0400

    operator functions work!

--- js/main.js
+++ js/main.js
@@ -10,6 +10,46 @@ function updateDisplay(){
 
   displayScreen.innerHTML = calculator.displayValue
 }
-updateDisplay()
+updateDisplay();
 
-test for github
+function inputDigits (digit){
+  const {displayValue} = calculator;
+
+  calculator.displayValue = displayValue === '0'? digit :  displayValue + digit
+}
+
+function inputDecimal(dot){
+  if(!calculator.displayValue.includes(dot)){
+    calculator.displayValue += dot;
+  }
+}
+
+
+function operatorRun(nextOperator){
+  const{firstOperand, displayValue, operator} = calculator
+}
+const keys = document.querySelector('.buttons');
+keys.addEventListener('click', (event) => {
+  const {target} = event;
+
+  if(target.classList.contains('digit')){
+    console.log('digit', target.innerHTML);
+    inputDigits(target.innerHTML);
+    updateDisplay();
+    return;
+  } else if(target.classList.contains('operator')){
+    console.log('operator', target.innerHTML);
+    return;
+  } else if(target.classList.contains('allClear')){
+    console.log('clear', target.innerHTML);
+    return;
+  } else if(target.classList.contains('decimal')){
+    console.log('decimal', target.innerHTML);
+    inputDecimal(target.innerHTML);
+    updateDisplay();
+    return;
+  } else {
+    return;
+  }
+});
+
